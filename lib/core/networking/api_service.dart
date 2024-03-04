62ed267ec7876f7845b9af94632b6584e89379a1
elesawy <elesawy928@gmail.com>
2024-03-05 00:18:18 +0200

    create login repo and create login cubit

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -2,15 +2,16 @@ import 'package:dio/dio.dart';
 import 'package:doc_doc_app/features/login/data/models/login_request.dart';
 
 import '../../features/login/data/models/login_response.dart';
+import 'api_constants.dart';
 
 class ApiService {
-  final String _baseUrl = 'https://vcare.integration25.com/api/';
   final Dio dio;
 
   ApiService(this.dio);
 
   Future<LoginResopnseBody> login(LoginRequestBody loginRequest) async {
-    var response = await dio.post('${_baseUrl}auth/login');
+    var response =
+        await dio.post('${ApiConstants.apiBaseUrl}${ApiConstants.login}');
     return response.data;
   }
 }
---
+++ lib/features/login/data/repo/login_repo.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:doc_doc_app/core/networking/api_error_handler.dart';
+import 'package:doc_doc_app/core/networking/api_service.dart';
+import 'package:doc_doc_app/features/login/data/models/login_request.dart';
+import 'package:doc_doc_app/features/login/data/models/login_response.dart';
+
+class LoginRepo {
+  final ApiService _apiService;
+
+  LoginRepo(this._apiService);
+
+  Future<Either<ErrorHandler, LoginResopnseBody>> login(
+      LoginRequestBody loginRequest) async {
+    try {
+      var response = await _apiService.login(loginRequest);
+
+      return right(response);
+    } catch (e) {
+      return left(ErrorHandler.handle(e));
+    }
+  }
+}
